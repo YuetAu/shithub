e64f6067711f8048d2f6e03587102a55ce30d840
YuetAu <sam07@live.hk>
2024-09-07 21:41:13 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,35 +17,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
-  const [auth, dispatch] = useReducer(authReducer, initialAuth);
+  
 
   return (
     <html>
       <body>
         <Providers>
-          <AuthContext.Provider value={auth}>
-            <AuthDispatchContext.Provider value={dispatch}>
               {children}
-            </AuthDispatchContext.Provider>
-          </AuthContext.Provider>
+           
         </Providers>
       </body>
     </html>
   )
 }
 
-const authReducer = (state: any, action: any) => {
-  switch (action.type) {
-    case 'LOGIN':
-      return { auth: true, user: action.payload }
-    case 'LOGOUT':
-      return { auth: false, user: null }
-    default:
-      return state
-  }
-}
-
-const initialAuth = {
-  auth: false,
-  user: null,
-}
