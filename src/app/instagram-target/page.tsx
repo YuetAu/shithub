7d4e2d23ebc8e476e17bc43f145cacfbe2f7c4a5
YuetAu <sam07@live.hk>
2024-10-01 23:03:07 +0800

    push instagram trap page

---
+++ src/app/instagram-target/page.tsx
@@ -0,0 +1,71 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+import {
+    Box,
+    Flex,
+    Text,
+    Image,
+    VStack,
+    GridItem,
+    Grid,
+    useColorModeValue,
+} from '@chakra-ui/react';
+
+const InstagramBrowserDetector = () => {
+    const [isInstagramBrowser, setIsInstagramBrowser] = useState(false);
+
+    useEffect(() => {
+        const checkBrowser = () => {
+            const userAgent = navigator.userAgent;
+            return userAgent.indexOf('Instagram') > -1;
+        };
+
+        setIsInstagramBrowser(checkBrowser());
+    }, []);
+
+    const bg = useColorModeValue('rgba(0,0,0,0.8)', 'rgba(255,255,255,0.8)');
+    const color = useColorModeValue('white', 'black');
+
+    /* if (!isInstagramBrowser) {
+        return null;
+    } */
+
+    return (
+        <Box
+            position="fixed"
+            top="0"
+            left="0"
+            right="0"
+            bottom="0"
+            bg={bg}
+            zIndex="overlay"
+        >
+            <Grid
+                templateRows="repeat(3, 1fr)"
+                height="100vh"
+                gap={4}
+                px={4}
+            >
+                <GridItem rowSpan={2}>
+                    <Flex h="100%" justifyContent="center" alignItems="center" textAlign="center">
+                        <Text color={color} fontSize="50" fontWeight="700" zIndex="99">
+                            請使用外置瀏覽器開啟
+                        </Text>
+                    </Flex>
+                </GridItem>
+                <GridItem>
+                    <Flex h="100%" justifyContent="center" alignItems="center" textAlign="center">
+                        <Text color={color} fontSize="100" fontWeight="1000" zIndex="99">
+                            💩
+                        </Text>
+                        <Text color={color} fontSize="50" fontWeight="100" zIndex="99">
+                            ShitHub
+                        </Text>
+                    </Flex>
+                </GridItem>
+            </Grid>
+        </Box>
+    );
+};
+
+export default InstagramBrowserDetector;
